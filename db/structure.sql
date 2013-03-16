7d9f4e69a443a5146a984212d450fcea2e2d22d2
Jon Morton <creator@agree.io>
2013-03-16 04:24:43 +0000

    Use SQL not schema.rb

---
+++ db/structure.sql
@@ -0,0 +1,183 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: participants; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE participants (
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    term_id uuid,
+    email text,
+    created_at timestamp without time zone
+);
+
+
+--
+-- Name: revisions; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE revisions (
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    term_id uuid,
+    participant_id uuid,
+    content text,
+    created_at timestamp without time zone
+);
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE schema_migrations (
+    version character varying(255) NOT NULL
+);
+
+
+--
+-- Name: signatures; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE signatures (
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    participant_id uuid NOT NULL,
+    inet_address inet NOT NULL,
+    mac_address macaddr NOT NULL,
+    created_at timestamp without time zone
+);
+
+
+--
+-- Name: terms; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE terms (
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    created_at timestamp without time zone
+);
+
+
+--
+-- Name: participants_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY participants
+    ADD CONSTRAINT participants_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: revisions_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY revisions
+    ADD CONSTRAINT revisions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: signatures_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY signatures
+    ADD CONSTRAINT signatures_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: terms_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY terms
+    ADD CONSTRAINT terms_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: index_participants_on_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_participants_on_email ON participants USING btree (email);
+
+
+--
+-- Name: index_participants_on_term_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_participants_on_term_id ON participants USING btree (term_id);
+
+
+--
+-- Name: index_revisions_on_term_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_revisions_on_term_id ON revisions USING btree (term_id);
+
+
+--
+-- Name: index_signatures_on_participant_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_signatures_on_participant_id ON signatures USING btree (participant_id);
+
+
+--
+-- Name: unique_schema_migrations; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE UNIQUE INDEX unique_schema_migrations ON schema_migrations USING btree (version);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+SET search_path TO "$user",public;
+
+INSERT INTO schema_migrations (version) VALUES ('20130315200200');
+
+INSERT INTO schema_migrations (version) VALUES ('20130315200202');
+
+INSERT INTO schema_migrations (version) VALUES ('20130315200209');
+
+INSERT INTO schema_migrations (version) VALUES ('20130315200215');
+
+INSERT INTO schema_migrations (version) VALUES ('20130315200221');
