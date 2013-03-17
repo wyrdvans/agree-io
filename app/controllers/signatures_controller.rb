647945c21109edf6f10adcd91a0505732faf1dce
Jon Morton <creator@agree.io>
2013-03-17 16:21:13 +0000

    Add polish to signature process

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -4,6 +4,8 @@ class SignaturesController < ApplicationController
 
   before_filter :enforce_required_parameters, except: 'show'
 
+  before_filter :detect_previous_signature, only: [ 'new', 'create' ]
+
   def show
     @signature = Signature.where(id: params[:id]).first
   end
@@ -15,10 +17,20 @@ class SignaturesController < ApplicationController
 
   def create
     @signature = Signature.create!(term: term, participant: participant)
-    respond_with @signature, location: @signature, status: '201'
+    respond_with @signature, location: @signature, status: '201' do |format|
+      format.html { redirect_to @signature.term }
+    end
   end
 
   protected
+
+  def detect_previous_signature
+    signature = Signature.where(term_id: term, participant_id: participant).first
+    if signature.present?
+      flash[:error] = "The terms were already signed by you."
+      redirect_to signature
+    end
+  end
 
   def enforce_required_parameters
     return true if term.present? && participant.present?
--- app/helpers/terms_helper.rb
+++ app/helpers/terms_helper.rb
@@ -1,2 +1,12 @@
 module TermsHelper
+
+  def signature_link term, email
+    participant = @term.participants.where(email: current_email).first
+    if participant.signature.present?
+      link_to "View Signature", participant.signature
+    else
+      link_to "Leave Signature", new_signature_path(participant_id: participant, term_id: term.id)
+    end
+  end
+
 end
