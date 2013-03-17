2353d00faf5f0c60ca2c96ed1508b2e9e44f217b
Jon Morton <creator@agree.io>
2013-03-17 11:50:26 +0000

    Clean up time format

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,7 @@ module Agree
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
 
     # Using PostgreSQL extensions means using a SQL dump.
     config.active_record.schema_format = :sql
